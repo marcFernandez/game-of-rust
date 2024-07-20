8ca87e668fee4d9c44d86e2faf6e750df9c9f5d9
Marc Fernandez Font <markiff@gmail.com>
2024-07-20 16:17:36 -0700

    feat: Client args for host and port

--- src/client/bin/client.rs
+++ src/client/bin/client.rs
@@ -3,14 +3,44 @@ use gol_multi::{
     term::{clear_terminal, render},
 };
 use std::{
+    env::args,
     io::{Read, Result},
     net::TcpStream,
+    process::exit,
 };
 
 fn main() -> Result<()> {
+    let mut args = args().skip(1);
+    let mut host = String::from("0.0.0.0");
+    let mut port = String::from("42069");
+    while let Some(next) = args.next() {
+        match next.as_str() {
+            "-p" => match args.next() {
+                Some(p) => {
+                    port = p;
+                }
+                None => {
+                    eprintln!("ERROR - Port expected after flag -p");
+                    exit(1);
+                }
+            },
+            "-h" => match args.next() {
+                Some(h) => {
+                    host = h;
+                }
+                None => {
+                    eprintln!("ERROR - Host expected after flag -h");
+                    exit(1);
+                }
+            },
+            _ => {
+                eprintln!("Unrecognized arg: {}", next);
+                exit(1)
+            }
+        }
+    }
     unsafe {
-        let stream = TcpStream::connect("0.0.0.0:42069").unwrap();
-        println!("Connection established from {}", stream.peer_addr().unwrap());
+        let stream = TcpStream::connect(format!("{host}:{port}")).unwrap();
         handle_connection(stream)?;
     }
 
@@ -24,7 +54,6 @@ unsafe fn handle_connection(mut stream: TcpStream) -> Result<()> {
     loop {
         stream.read_exact(&mut buffer)?;
         let grid_data = String::from_utf8(buffer.to_vec()).unwrap();
-        eprintln!("Grid: {}", grid_data);
 
         grid_data.chars().enumerate().for_each(|(i, val)| {
             GRID[i] = val.to_string().parse().unwrap();
